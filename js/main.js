91298c64f843c5290e227460bc2a7c6440902467
Evil-Pinguin <evil-pinguin@github.com>
2026-07-26 16:43:29 +0900

    feat: add menu section with CSS Grid and like button logic

---
+++ js/main.js
@@ -0,0 +1,17 @@
+const likeButtons = document.querySelectorAll('.card__like');
+
+likeButtons.forEach(button => {
+
+    button.addEventListener('click', function(){
+        const currentText = this.textContent;
+
+        let likesCount = parseInt(currentText.split(' ')[1]);
+
+        likesCount++; 
+
+        this.textContent = `♥ ${likesCount}`;
+
+        this.classList.toggle('card__like_active');
+    }
+);
+});
